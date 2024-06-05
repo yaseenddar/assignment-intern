7f63c602157758142b382219c5edd0eb9840e1f2
yaseen@12 <yaseen@gmail.com>
2024-06-06 00:06:55 +0530

    added the redux to vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
 
-// https://vitejs.dev/config/
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      external: ['react', 'react-dom', 'react-redux'],
+    },
+  },
+});
